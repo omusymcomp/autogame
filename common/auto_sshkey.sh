ee5b483ec9e14eecbe497846dd76b16eef7f1353
hishiki <scatrna2631@gamil.com>
2024-08-09 11:40:39 +0900

    Fully automated(No need to enter a password)

--- common/auto_sshkey.sh
+++ common/auto_sshkey.sh
@@ -2,14 +2,14 @@
 
 source ./hostnames
 
-read -sp "Password: " pswd
+# read -sp "Password: " pswd
 
 echo ""
 echo "# -------------------------------- #"
 echo "  server"
 echo "  install ssh..."
 echo "# -------------------------------- #"
-sudo apt-get install ssh openssh-server sshpass
+echo "${server_pswd}" | sudo -S apt-get install -y ssh openssh-server sshpass
 
 echo "# -------------------------------- #"
 echo "  clients"
@@ -18,8 +18,13 @@ echo "# -------------------------------- #"
 for i in "${hostnames_ids[@]}";
 do
 	hst=(${i[@]})
+	user_check=$(echo ${pswd} | sshpass -p ${pswd} ssh -o StrictHostKeyChecking=no -o PreferredAuthentications=password ${shared_username}@${network}${hst[1]} id -u ${username} > /dev/null 2>&1; echo $?)
+	if [ $user_check -eq 0 ]; then
+    	echo "User ${username} exists on ${network}${hst[1]}."
+		continue
+	fi
 	echo ${shared_username}@${network}${hst[1]}
-	echo ${pswd} | sshpass -p ${pswd} ssh -o StrictHostKeyChecking=no -o PreferredAuthentications=password "${shared_username}@${network}${hst[1]}" "sudo -S adduser --disabled-password --gecos '' ${username}; echo ${username}:${pswd} | sudo -S chpasswd; sudo -S gpasswd -a ${username} sudo; sudo -S apt-get install ssh openssh-server;"
+	echo ${pswd} | sshpass -p ${pswd} ssh -o StrictHostKeyChecking=no -o PreferredAuthentications=password "${shared_username}@${network}${hst[1]}" "echo ${pswd} | sudo -S adduser --disabled-password --gecos '' ${username}; echo ${username}:${pswd} | sudo -S chpasswd; sudo -S gpasswd -a ${username} sudo; sudo -S apt-get install -y ssh openssh-server;"
 	sshpass -p ${pswd} ssh -t -o StrictHostKeyChecking=no -o PreferredAuthentications=password "${username}@${network}${hst[1]}" "mkdir .ssh"
 done
 
@@ -44,6 +49,7 @@ do
 		echo "  User ${username}"
 		echo "  IdentityFile ~/.ssh/id_rsa_${hst[0]}"
 		echo "  ServerAliveInterval 60"
+		echo "  IdentitiesOnly yes"
 	} >> config
 done
 
@@ -54,11 +60,13 @@ echo "# -------------------------------- #"
 for i in "${hostnames_ids[@]}";
 do
 	hst=(${i[@]})
-	if [ -f ~/.ssh/id_rsa_${hst[0]}.pub ]; then
-		sshpass -p ${pswd} ssh -o StrictHostKeyChecking=no -o PreferredAuthentications=password "${username}@${network}${hst[1]}" "cd ~/.ssh/; cat id_rsa_${hst[0]}.pub >> authorized_keys; rm id_rsa_${hst[0]}.pub;"
-	else
+	key_file="~/.ssh/id_rsa_${hst[0]}.pub"
+	pub_check=$(sshpass -p "${pswd}" ssh -o StrictHostKeyChecking=no -o PreferredAuthentications=password "${username}@${network}${hst[1]}" "[ -f ${key_file} ] && echo 'exists' || echo 'does_not_exist'")
+	if [ $pub_check = "does_not_exist" ]; then
 		echo "Key authorization is already finished..."
 		echo "skip ${hst[0]}"
+	else
+		sshpass -p ${pswd} ssh -o StrictHostKeyChecking=no -o PreferredAuthentications=password "${username}@${network}${hst[1]}" "cd ~/.ssh/; cat id_rsa_${hst[0]}.pub >> authorized_keys; rm id_rsa_${hst[0]}.pub;"
 	fi
 
 done
